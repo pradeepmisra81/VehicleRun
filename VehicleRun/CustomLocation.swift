61610c1eeb02e5b440c5ffdbac227e2e4bad03ce
pradeep kumar misra <pradeep.misra@gmail.com>
2017-06-26 01:49:05 +0530

    add CustomLocation.swift file

---
+++ VehicleRun/CustomLocation.swift
@@ -0,0 +1,19 @@
+//
+//  CustomLocation.swift
+//  VehicleRun
+//
+//  Created by pradeep kumar misra on 25/06/17.
+//  Copyright © 2017 pradeep kumar misra. All rights reserved.
+//
+
+import Foundation
+
+struct CustomLocation {
+    
+    var timestamp: Date
+    var latitude: Double
+    var longitude: Double
+    var currenttimeinterval: Double
+    var nexttimeinterval: Double
+}
+
